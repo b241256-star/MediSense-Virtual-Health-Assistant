adc461f7b299042b3d0c34ba0c9881f5e1d3f1d6
Sakshi Agarwal <b241256@skit.ac.in>
2026-04-18 13:46:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -276,10 +276,4 @@ async function loadSession(sid) {
     });
   }
 }
-<script>
-history.pushState(null, null, location.href);
 
-window.onpopstate = function () {
-    history.pushState(null, null, location.href);
-};
-</script>
